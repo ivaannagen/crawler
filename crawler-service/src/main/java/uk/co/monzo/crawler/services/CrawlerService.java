42469126d6fffbef0bc1f0118275279854c4d5ec
Ivaan Nagen <ivaannagen@gmail.com>
2025-02-15 15:48:57 +0000

    both solutions

--- crawler-service/src/main/java/uk/co/monzo/crawler/services/CrawlerService.java
+++ crawler-service/src/main/java/uk/co/monzo/crawler/services/CrawlerService.java
@@ -3,6 +3,7 @@ package uk.co.monzo.crawler.services;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.collections.CollectionUtils;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -10,10 +11,7 @@ import org.springframework.stereotype.Service;
 import uk.co.monzo.crawler.client.JsoupClient;
 import uk.co.monzo.crawler.repository.CrawlerRepository;
 
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -38,7 +36,7 @@ public class CrawlerService {
     }
 
     public Map<String, Set<String>> fetchUrls(String url, int maxLevel) {
-        return fetchUrls(url, url, 1, maxLevel);
+        return fetchUrls(url, url, new HashMap<>(), 1, maxLevel);
     }
 
 
@@ -64,36 +62,34 @@ public class CrawlerService {
         return crawlerRepository.getCache();
     }
 
-//    private HashMap<String, Set<String>> fetchUrls(String url, String baseUrl, HashMap<String, Set<String>> visited, int level, int maxLevel) {
-//
-//        if (level <= maxLevel) {
-//
-//            executor.submit(new CrawlerWorker(jsoupClient));
-//
-//
-//            Optional<Document> nodeOpt = jsoupClient.getDocument(url);
-//
-//            if (nodeOpt.isPresent()) {
-//                Document node = nodeOpt.get();
-//
-//                Set<String> visitedNodes = new LinkedHashSet<>();
-//                visited.putIfAbsent(url, visitedNodes);
-//
-//                for (Element element : node.select("a[href]")) {
-//                    String nextLink = element.attr("abs:href");
-//
-//                    if (!visited.containsKey(nextLink) && nextLink.startsWith(baseUrl)) {
-//                        nextLinks.add(nextLink);
-//                        fetchUrls(nextLink, baseUrl, level++, maxLevel);
-//                    }
-//                }
-//                visited.computeIfPresent(url, (k, v) -> nextLinks);
-//            }
-//
-//        }
-//
-//        return visited;
-//    }
+    private HashMap<String, Set<String>> fetchUrls(String url, String baseUrl, HashMap<String, Set<String>> visited, int level, int maxLevel) {
+
+        if (level <= maxLevel) {
+
+
+            Optional<Document> nodeOpt = jsoupClient.getDocument(url);
+
+            if (nodeOpt.isPresent()) {
+                Document node = nodeOpt.get();
+
+                Set<String> visitedNodes = new LinkedHashSet<>();
+                visited.putIfAbsent(url, visitedNodes);
+
+                for (Element element : node.select("a[href]")) {
+                    String nextLink = element.attr("abs:href");
+
+                    if (!visited.containsKey(nextLink) && nextLink.startsWith(baseUrl)) {
+                        visitedNodes.add(nextLink);
+                        fetchUrls(nextLink, baseUrl, visited, level++, maxLevel);
+                    }
+                }
+                visited.computeIfPresent(url, (k, v) -> visitedNodes);
+            }
+
+        }
+
+        return visited;
+    }
 
 
     private Set<String> fetchUrls(String url) {
